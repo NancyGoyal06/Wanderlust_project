975cd03a16ceb1d504fa3e92aa33403b977883e8
Nancy goyal <nancygoyal.bg@gmail.com>
2025-07-23 00:29:10 +0530

    added flash messages

--- routes/listing.js
+++ routes/listing.js
@@ -31,6 +31,10 @@ router.get("/new",(req,res) => {
 router.get("/:id",wrapAsync( async (req,res) => {
         let {id} = req.params;
         const listing =await Listing.findById(id).populate("reviews");
+        if(!listing){
+            req.flash("error", "listing you requested for does not exist!");
+            res.redirect("/listings");
+        }
         res.render("listings/show.ejs", { listing });
     })
 );
@@ -39,6 +43,7 @@ router.get("/:id",wrapAsync( async (req,res) => {
 router.post("/", validateListing ,wrapAsync( async (req,res,next)=>{
         const newListing = new Listing(req.body.listing);
         await newListing.save();
+        req.flash("success", "New listing created");
         res.redirect("/listings");
     })
 );
@@ -47,6 +52,10 @@ router.post("/", validateListing ,wrapAsync( async (req,res,next)=>{
 router.get("/:id/edit", wrapAsync( async (req,res) => {
         let { id } = req.params;
         const listing =await Listing.findById(id);
+        if(!listing){
+            req.flash("error", "listing you requested for does not exist!");
+            res.redirect("/listings");
+        }
         res.render("listings/edit.ejs",{ listing });
     })
 );
@@ -55,6 +64,7 @@ router.get("/:id/edit", wrapAsync( async (req,res) => {
 router.put("/:id", validateListing ,wrapAsync( async (req,res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    req.flash("success", "listing Updated");
     res.redirect(`/listings/${id}`);
 })
 );
@@ -64,6 +74,7 @@ router.delete("/:id",wrapAsync( async (req,res) =>{
         let { id } = req.params;
         let deletedListing = await Listing.findByIdAndDelete(id);
         console.log(deletedListing);
+        req.flash("success", "Listing Deleted");
         res.redirect("/listings");
 })
 );
